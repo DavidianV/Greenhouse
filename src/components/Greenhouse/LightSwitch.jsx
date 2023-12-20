b115b00d02f4381542bcf9dd5ad6dcb4f8706bbc
David <david@vances.org>
2023-12-20 18:35:10 -0500

    troubleshooting

--- src/components/Greenhouse/LightSwitch.jsx
+++ src/components/Greenhouse/LightSwitch.jsx
@@ -1,10 +1,18 @@
 import './LightSwitch.css';
+import { useTheme } from '../../context/ThemeContext'
 
 function LightSwitch() {
+
+  const { themeName, setThemeName } = useTheme()
+
+  const toggleTheme = (newTheme) => {
+    setThemeName(newTheme)
+  }
+  
   return (
-    <div className="light-switch day">
-      <div className="on">DAY</div>
-      <div className="off">NIGHT</div>
+    <div className={`light-switch ${themeName}`}>
+      <div className="on" onClick={() => {toggleTheme('day')}}>DAY</div>
+      <div className="off" onClick={() => {toggleTheme('night')}}>NIGHT</div>
     </div>
   );
 }
--- src/context/ClimateContext.jsx
+++ src/context/ClimateContext.jsx
@@ -1,2 +1,18 @@
 // Temperature has a default value of 50 degrees
 // Humidity has a default value of 40%
+import React, {createContext, useContext, useState} from "react";
+
+export const ClimateContext = createContext()
+
+export function ClimateProvider({children}) {
+    const [temperature, setTemperature] = useState(50)
+    const value = {temperature, setTemperature}
+    
+    return (
+        <ClimateContext.Provider value={value}>
+            {children}
+        </ClimateContext.Provider>
+    )
+}
+
+export const useClimate = () => {useContext(ClimateContext)}
